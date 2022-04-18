a38ee2dff7a7242c246acefd80b4d8385405195f
Evhenos <96354134+Evhenos@users.noreply.github.com>
2022-04-18 11:12:19 +0300

    Update wordli (2).py

--- wordli (2).py
+++ wordli (2).py
@@ -3,6 +3,9 @@
 from termcolor import colored
 import random
 import sys
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
 
 def read_random_word():
     with open("words.txt", encoding='utf-8') as f:
@@ -10,21 +13,22 @@ def read_random_word():
         return random.choice(word_array)
 
 print("Введите слово:")
-word = read_random_word()
+word = read_random_word().upper()
 
 for attempt in range(1,7):
-    guess = input().lower()
+    guess = input().upper()
     
  
     sys.stdout.write('\x1b[1A')
     sys.stdout.write('\x1b[2K')
+   
 
    
     for i in range(min(len(guess), 5)):
         if guess[i] == word[i]:
-            print(colored(guess[i], 'green'), end="")
+            print((Fore.GREEN + guess[i]), end="")
         elif guess[i] in word:
-            print(colored(guess[i], 'yellow'), end="")
+            print((Fore.YELLOWпа + guess[i]), end="")
         else:
             print(guess[i], end="")
     print()
@@ -33,8 +37,6 @@ for attempt in range(1,7):
         print('Поздравляем! Вы угадали слово за %i попытки.' %attempt)
     elif attempt == 6:
         print('Вы не угадали слово с 6 попыток, это было %s' %word.upper())
-
-
 
 
 
